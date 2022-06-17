3a0001da5c789d0f9493f17b7f8c76e1827977d7
Isaac Evelyn <isaacevelyn93@gmail.com>
2022-06-17 08:38:51 +0100

    updates

---
+++ script/main.js
@@ -0,0 +1,9 @@
+let formEl = document.getElementById("form")
+
+formEl.addEventListener("submit", function(ev){
+    ev.preventDefault()
+    console.log(formEl)
+    // for(let i = 0; i < input.length; i++){
+    //     console.log(i)
+    // }
+})
