676c2950ec162029bc6b73ffc8147ec318b49aef
Isaac Evelyn <isaacevelyn93@gmail.com>
2022-06-17 10:22:21 +0100

    updates

--- script/main.js
+++ script/main.js
@@ -1,9 +1,18 @@
-let formEl = document.getElementById("form")
+let formEl = document.getElementById("form_el")
+let btnEl = document.getElementById("btn_el")
+let input = document.querySelectorAll("input")
+// console.log(formEl)
 
-formEl.addEventListener("submit", function(ev){
+formEl.addEventListener("submit", function (ev) {
     ev.preventDefault()
-    console.log(formEl)
-    // for(let i = 0; i < input.length; i++){
-    //     console.log(i)
-    // }
+    for (let i = 0; i < input.length; i++) {
+        console.log(input[i])
+        if (input[i].value == '') {
+            input[i].placeholder = "Please fill this field"
+             input[i].style.border = "2px solid red"
+        } else {
+            console.log("Okay")
+        }
+    }
+
 })
