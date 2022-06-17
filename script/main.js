516dae71e183be2d9871665206490a1d9003072c
Isaac Evelyn <isaacevelyn93@gmail.com>
2022-06-17 17:14:34 +0100

    updates

--- script/main.js
+++ script/main.js
@@ -3,25 +3,37 @@ let btnEl = document.getElementById("btn_el")
 let input = document.querySelectorAll("input")
 let errorText = document.createElement("span")
 let errorIcon = document.querySelectorAll("img")
+let error = document.querySelectorAll(".err_msg")
+console.log(error)
 
 formEl.addEventListener("submit", function (ev) {
     ev.preventDefault()
     for (let i = 0; i < input.length; i++) {
 
-        errorText.textContent = "Field is empty"
-        errorText.style.color = "red"
-        
-        for(let icon = 0; icon < errorIcon.length; icon++){
-           errorIcon[icon].classList.remove("hide")
+        for (let icon = 0; icon < errorIcon.length; icon++) {
+          
+            for(let msg = 0; msg < error.length; msg++){
+
+                if (input[i].value == '') {
+
+                    input[i].style.border = "1px solid red"
+                    
+                    error[msg].classList.remove("hide") 
+
+                    errorIcon[icon].classList.remove("hide")
+    
+    
+                } else {
+    
+                    errorIcon[icon].classList.add("hide")
+    
+                    console.log("Okay")
+                }
+            }
+
         }
-        
-        if (input[i].value == '') {
-            input[i].placeholder = "Please fill this field"
-            input[i].style.border = "1px solid red"
-            input[i].append(errorText)
-        } else {
-            console.log("Okay")
-        }
+
+
     }
 
 })
